575e04c8026aa5bbe4c0198524c82bd84e74b901
moweeece <mjespinosa990@gmail.com>
2024-09-25 15:04:45 -0700

    Second Commit

--- main.cpp
+++ main.cpp
@@ -2,11 +2,40 @@
 #include <iomanip>
 using namespace std;
 
+// class Declaration
+class Movie
+{
+    private:
+    string title;
+    int year;
+    string writer;
+    
+    public:
+    // getters and setters
+    string getTitle()           { return title; }
+    void setTitle(string t)     { title = t; }
+    int getYear()               { return year; }
+    void setYear(int y)         { year = y; }
+    string getWriter()          { return writer; }
+    void setWriter(string w)    { writer = w; }
 
+
+    // print mehtod
+    void print()
+    {
+        cout << "Movie: " << title << endl;
+        cout << setw(15) << "Year: " << year << endl;
+        cout << setw(15) << "Writer: " << writer << endl << endl;
+    }
+
+};
 
 
 int main() {
 
+    // First movie object
+    Movie movie1;
+   
 
 
     return 0;
